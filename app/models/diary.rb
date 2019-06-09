83e32d418719e4ed78ee5bc64fdb7f8359d11d77
Yoshimasa Suzuki <cognac0004@gmail.com>
2019-06-09 18:38:45 +0900

    added validation

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,10 +1,13 @@
 class Diary < ApplicationRecord
   belongs_to :user
 
+  validates :content, presence: true
+  validates :user_id, presence: true
+
   def self.get_diaries(user_id)
     diaries = Diary.where(user_id: user_id).order(created_at: "ASC")
     diaries.map { |diary| {id: diary.id, content: diary.content, year: diary.created_at.strftime("%Y"),
                           month: diary.created_at.strftime("%-m"), day: diary.created_at.strftime("%d")} }
   end
-  
+
 end
--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,11 +1,14 @@
 class Memo < ApplicationRecord
   belongs_to :user
 
+  validates :content, presence: true
+  validates :user_id, presence: true
+
   def self.get_memos(user_id)
     today = Time.now
     today_range = today.beginning_of_day..today.end_of_day
     todays_memos = Memo.where(user_id: user_id, created_at: today_range)
     todays_memos.map { |memo| { id: memo.id, content: memo.content, time: memo.created_at.strftime("%H：%M") } }
   end
-  
+
 end
